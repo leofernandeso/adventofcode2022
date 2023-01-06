7485c404c9b737bda686c5bef3f7c2d9c3090ede
Leonardo Fernandes Oliveira <leonardo.oliveira@tum.de>
2023-01-06 17:52:23 +0100

    add day01

---
+++ advent_of_rust/src/bin/day1_2.rs
@@ -0,0 +1,53 @@
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_path = parse_arguments();
+    println!("{}", input_path);    
+    let lines = read_lines(input_path)?;
+
+    let total_of_top_three_carbs = find_total_of_top_three_carbs(lines)?;
+    println!("{}", total_of_top_three_carbs);
+
+    Ok(())
+}
+
+fn find_total_of_top_three_carbs(input_file_lines: io::Lines<BufReader<File>>) -> Result<u64, Box<dyn Error>> {
+    let mut carbs_vec = Vec::new();
+    let mut current_sum: u64 = 0;
+    for line_result in input_file_lines {
+        let carbs_amount_string = line_result?;
+        if carbs_amount_string.is_empty() {
+            carbs_vec.push(current_sum);
+            current_sum = 0;
+            continue;
+        }
+        let carbs_amount = carbs_amount_string.parse::<u64>()?;
+        current_sum += carbs_amount;
+    }
+
+    carbs_vec.sort();
+    Ok(carbs_vec[carbs_vec.len() - 3..].iter().sum())
+
+}
+
+fn read_lines(filename: String) -> Result<io::Lines<io::BufReader<File>>, io::Error> {
+    let file_result = File::open(filename);
+    match file_result {
+        Ok(file) => Ok(io::BufReader::new(file).lines()),
+        Err(_) => Err(io::Error::new(io::ErrorKind::NotFound, "File not found. Please specify a valid file path")),
+    }
+}
+
+fn parse_arguments() -> String {
+    
+    if let Some(input_path) = std::env::args().nth(1) {
+        input_path
+    } else {
+        eprintln!("ERROR: Provide the input path!!!");
+        std::process::exit(1);
+    }
+
+}
